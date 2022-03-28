3767f16ffb53b74a4829468398d557bd577a021c
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-28 16:16:22 -0700

    finished episode testing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -9,13 +9,20 @@ const testEpisode = {
     image: "https://i.ibb.co/2FsfXqM/stranger-things.png", 
     season: 1, 
     number: 1, 
-    summary: "", 
+    summary: "test summary", 
     runtime: 1
 
 }
 
 
 const testEpisodeWithoutImage = {
+    id: 1, 
+    name: "", 
+    image: null, 
+    season: 1, 
+    number: 1, 
+    summary: "test summary", 
+    runtime: 1
 
 }
 
@@ -23,6 +30,20 @@ test("renders without error", () => {
     render(<Episode episode ={testEpisode} /> )
 });
 
-test("renders the summary test passed as prop", () => { });
+test("renders the summary test passed as prop", () => {
+    render( <Episode  episode = {testEpisode}/> )
+    const summary = screen.queryByText(/test summary/i); 
+    expect(summary).toBeInTheDocument()
+    expect(summary).toBeTruthy()
+    expect(summary).toHaveTextContent('test summary')
+ });
 
-test("renders default image when image is not defined", () => { });
+test("renders default image when image is not defined", () => {
+    render(<Episode episode = {testEpisodeWithoutImage}/> )
+    const image = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+
+    expect(image).toBeInTheDocument()
+    expect(image).toBeTruthy()
+    expect(image).toBeVisible()
+
+ });
