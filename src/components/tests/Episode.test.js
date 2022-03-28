f8ee11ff273d7912635f57617cee127cd4bb87f0
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-28 15:41:54 -0700

    test episode data is created  and Epsiode is rendering

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,7 +3,25 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisode = {
+    id: 1, 
+    name: "", 
+    image: "https://i.ibb.co/2FsfXqM/stranger-things.png", 
+    season: 1, 
+    number: 1, 
+    summary: "", 
+    runtime: 1
+
+}
+
+
+const testEpisodeWithoutImage = {
+
+}
+
+test("renders without error", () => { 
+    render(<Episode episode ={testEpisode} /> )
+});
 
 test("renders the summary test passed as prop", () => { });
 
