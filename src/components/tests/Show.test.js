ed0f6d3befd22281032215b0c38c256dab2c79cd
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-28 16:20:42 -0700

    created my test show object

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -3,6 +3,20 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Show from './../Show';
 
+const testShow = {
+    name: 'test show', 
+    summary: 'test summary', 
+    season: [{
+        id: 0, 
+        name: 'Season 1', 
+        episode: []
+    }, {
+        id: 1, 
+        name: 'Season 2', 
+        episode: []
+    }]
+}
+
 test('renders without errors', () => { });
 
 test('renders Loading component when prop show is null', () => { });
